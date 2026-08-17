f87e7f3a5ca269d10e6f98517b0037d1ad905bd7
fengharry <feng.harrybj@gmail.com>
2026-08-16 22:31:59 -0500

    testing commit

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 int main() {
-    ShutTheBox g(13);
+    ShutTheBox g(8);
     SaveOne o;
     LargestNumber l;
     MostNumbers m;
-    g.probability_of_strategy_victory(&o, "save_one_13.csv");
-    g.probability_of_strategy_victory(&l, "largest_number_13.csv");
-    g.probability_of_strategy_victory(&m, "most_numbers_13.csv");
+    g.probability_of_strategy_victory(&o, "save_one_8.csv");
+    g.probability_of_strategy_victory(&l, "largest_number_8.csv");
+    g.probability_of_strategy_victory(&m, "most_numbers_8.csv");
     g.probability_of_optimal_victory(WIN_PROBABILITY);
     g.probability_of_optimal_victory(AVERAGE_SCORE);
 }
